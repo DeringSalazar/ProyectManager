f11eca4412935fc2b742371253bdb63224efd03c
Keizel <pamelamarchena76@gmail.com>
2024-08-01 16:44:26 -0600

    Clase Projects
    Se crea la clase projects con los atributos, metodos y constructores

---
+++ Projects/Project.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Projects;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pamel
+ */
+public class Project {
+    private int id;
+    private String name;
+    private ArrayList<Task>Task;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Task> getTask() {
+        return Task;
+    }
+    
+    public void addTasks(Task task){
+        this.addTasks(Task);
+    }
+
+    public Project(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.Task = new ArrayList<>();
+    }
+
+    public Project() {
+        this(0, "");
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" + "id=" + id + ", name=" + name + ", Task=" + Task + '}';
+    }
+    
+    
+}
